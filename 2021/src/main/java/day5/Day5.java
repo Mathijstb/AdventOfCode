85e847315928ea2c6b6099b45747109cb6129353
Mathijs ter Braak <mathijstb@gmail.com>
2021-12-04 09:36:57 +0100

    Advent Of Code - 2021
    Day4

---
+++ 2021/src/main/java/day5/Day5.java
@@ -0,0 +1,14 @@
+package day5;
+
+import fileUtils.FileReader;
+
+import java.util.List;
+
+public class Day5 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input5.csv");
+
+    }
+
+}
