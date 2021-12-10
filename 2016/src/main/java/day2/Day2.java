a0ef32ef3576ae85bae155f0c69ed5532a82aae8
Mathijs ter Braak <mathijstb@gmail.com>
2021-12-10 22:35:43 +0100

    Advent Of Code - 2016
    Day 2

---
+++ 2016/src/main/java/day2/Day2.java
@@ -0,0 +1,13 @@
+package day2;
+
+import fileUtils.FileReader;
+
+import java.util.List;
+
+public class Day2 {
+
+    public static void execute() {
+        List<String> input = FileReader.getFileReader().readFile("input2.csv");
+
+    }
+}
