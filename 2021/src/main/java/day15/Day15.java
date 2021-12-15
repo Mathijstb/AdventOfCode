1d0aa0829e3397162403e38e76ba6d664c47f08b
Mathijs ter Braak <mathijstb@gmail.com>
2021-12-15 07:37:04 +0100

    Advent Of Code - 2021
    Day 15

---
+++ 2021/src/main/java/day15/Day15.java
@@ -0,0 +1,13 @@
+package day15;
+
+import fileUtils.FileReader;
+
+import java.util.List;
+
+public class Day15 {
+
+    public static void execute() {
+        List<String> input = FileReader.getFileReader().readFile("input15.csv");
+    }
+
+}
