c716c35a5b6601f4b4cd9adf5196b1b3815a09c4
Mathijs ter Braak <mathijstb@gmail.com>
2021-12-19 12:14:37 +0100

    Advent Of Code - 2021
    Day 18

--- 2021/src/main/java/day18/Day18.java
+++ 2021/src/main/java/day18/Day18.java
@@ -2,6 +2,7 @@ package day18;
 
 import fileUtils.FileReader;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,7 +21,74 @@ public class Day18 {
     }
 
     private static Pair reducePair(Pair pair) {
+        Optional<Pair> explodable = findExplodable(pair);
+        Optional<Pair> splittable = findSplittable(pair);
+        while (explodable.isPresent() || splittable.isPresent()) {
+            while (explodable.isPresent()) {
+                explode(explodable.get());
+                explodable = findExplodable(pair);
+            }
+            while (splittable.isPresent()) {
+                split(splittable.get());
+                splittable = findSplittable(pair);
+            }
+            explodable = findExplodable(pair);
+            splittable = findSplittable(pair);
+        }
         return pair;
+    }
+
+    private static Optional<Pair> findExplodable(Pair pair) {
+        return getPairs(pair, false).stream()
+                .filter(p -> p.getNestLevel() >= 4)
+                .filter(p -> p.left.orElseThrow().numericalValue.isPresent())
+                .filter(p -> p.right.orElseThrow().numericalValue.isPresent()).findFirst();
+    }
+
+    private static Optional<Pair> findSplittable(Pair pair) {
+        return getPairs(pair, true).stream()
+                .filter(p -> p.numericalValue.isPresent() && p.numericalValue.get() >= 10).findFirst();
+    }
+
+    private static void explode(Pair pair) {
+        pair.findLeftNumericalValue(true).ifPresent(value -> value.addValue(pair.left.orElseThrow().numericalValue.orElseThrow()));
+        pair.findRightNumericalValue(true).ifPresent(value -> value.addValue(pair.right.orElseThrow().numericalValue.orElseThrow()));
+        Pair ancestor = pair.ancestor.orElseThrow();
+        if (ancestor.left.orElseThrow().equals(pair)) {
+            ancestor.setLeft(Optional.of(new Pair(Optional.of(0), Optional.empty(), Optional.empty(), Optional.of(ancestor))));
+        }
+        else {
+            ancestor.setRight(Optional.of(new Pair(Optional.of(0), Optional.empty(), Optional.empty(), Optional.of(ancestor))));
+        }
+    }
+
+    private static void split(Pair pair) {
+        int value = pair.getNumericalValue().orElseThrow();
+        int leftValue = value / 2;
+        int rightValue = (value / 2) * 2 == value ? value / 2 : value / 2 + 1;
+        Pair newPair = combinePair(new Pair(Optional.of(leftValue), Optional.empty(), Optional.empty()),
+                                   new Pair(Optional.of(rightValue), Optional.empty(), Optional.empty()));
+        Pair ancestor = pair.getAncestor().orElseThrow();
+        newPair.setAncestor(Optional.of(ancestor));
+        if (ancestor.left.orElseThrow() == pair) {
+            ancestor.setLeft(Optional.of(newPair));
+        }
+        else {
+            ancestor.setRight(Optional.of(newPair));
+        }
+    }
+
+    private static List<Pair> getPairs(Pair pair, boolean includeNumerical) {
+        List<Pair> result = new ArrayList<>();
+        if (pair.numericalValue.isPresent()) {
+            if (includeNumerical) result.add(pair);
+        }
+        else {
+            result.add(pair);
+            result.addAll(getPairs(pair.left.orElseThrow(), includeNumerical));
+            result.addAll(getPairs(pair.right.orElseThrow(), includeNumerical));
+        }
+        return result;
     }
 
     private static Pair readPair(String expression) {
--- 2021/src/main/java/day18/Pair.java
+++ 2021/src/main/java/day18/Pair.java
@@ -16,12 +16,72 @@ public class Pair {
 
     Optional<Pair> right;
 
-    Optional<Pair> ancestor;
+    Optional<Pair> ancestor = Optional.empty();
 
     public Pair(Optional<Integer> numericalValue, Optional<Pair> left, Optional<Pair> right) {
         this.numericalValue = numericalValue;
         this.left = left;
         this.right = right;
+    }
+
+    public void addValue(int value) {
+        setNumericalValue(Optional.of(numericalValue.orElseThrow() + value));
+    }
+
+    public int getNestLevel() {
+        return ancestor.map(a -> a.getNestLevel() + 1).orElse(0);
+    }
+
+    public boolean isLeftPair() {
+        return ancestor.isPresent() && ancestor.get().left.orElseThrow().equals(this);
+    }
+
+    public boolean isRightPair() {
+        return ancestor.isPresent() && ancestor.get().right.orElseThrow().equals(this);
+    }
+
+    private Pair findLeftMostNestedValue() {
+        if (numericalValue.isPresent()) return this;
+        return left.orElseThrow().findLeftMostNestedValue();
+    }
+
+    private Pair findRightMostNestedValue() {
+        if (numericalValue.isPresent()) return this;
+        return right.orElseThrow().findRightMostNestedValue();
+    }
+
+    public Optional<Pair> findLeftNumericalValue(boolean isRoot) {
+        if (!isRoot && numericalValue.isPresent()) {
+            return Optional.of(this);
+        }
+        else {
+            if (isLeftPair()) {
+                return ancestor.orElseThrow().findLeftNumericalValue(false);
+            }
+            else if (isRightPair()) {
+                return Optional.of(ancestor.orElseThrow().left.orElseThrow().findRightMostNestedValue());
+            }
+            else {
+                return Optional.empty();
+            }
+        }
+    }
+
+    public Optional<Pair> findRightNumericalValue(boolean isRoot) {
+        if (!isRoot && numericalValue.isPresent()) {
+            return Optional.of(this);
+        }
+        else {
+            if (isLeftPair()) {
+                return Optional.of(ancestor.orElseThrow().right.orElseThrow().findLeftMostNestedValue());
+            }
+            else if (isRightPair()) {
+                return ancestor.orElseThrow().findRightNumericalValue(false);
+            }
+            else {
+                return Optional.empty();
+            }
+        }
     }
 
     @Override
