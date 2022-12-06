ba34bb37275c0bee3d90855e50be20c058b5754f
Mathijs ter Braak <38856699+Mathijstb@users.noreply.github.com>
2022-12-06 10:19:40 +0100

    day6 update

--- 2022/src/main/java/day6/Day6.java
+++ 2022/src/main/java/day6/Day6.java
@@ -3,7 +3,7 @@ package day6;
 import fileUtils.FileReader;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.regex.Pattern;
 
 public class Day6 {
 
@@ -15,15 +15,24 @@ public class Day6 {
     }
 
     private static void findFirstMarker(String line, int size) {
-        for (int i = 0; i < line.length()-size; i++) {
-            var characterSet = line.substring(i, i + size)
-                    .chars()
-                    .mapToObj(chr -> (char) chr)
-                    .collect(Collectors.toSet());
-            if (characterSet.size() == size) {
-                System.out.println("Index: " + (i + size));
-                break;
-            }
+        var regex = createRegex(size);
+        var pattern = Pattern.compile(regex);
+        var matcher = pattern.matcher(line);
+        if (matcher.find()) {
+            System.out.println("Index: " + matcher.end());
         }
+    }
+
+    private static String createRegex(int size) {
+        //e.g. for size 4: "(.)(?!\\1)(.)(?!\\1|\\2)(.)(?!\\1|\\2|\\3)(.)"
+        StringBuilder sb = new StringBuilder("(.)");
+        for (int i = 1; i < size; i++) {
+            sb.append("(?!\\1");
+            for (int j = 2; j < i+1; j++) {
+                sb.append("|\\").append(j);
+            }
+            sb.append(")(.)");
+        }
+        return sb.toString();
     }
 }
