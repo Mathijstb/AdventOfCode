e685602f4bf5f41c3636daa56faec2bea808f0a7
Mathijs ter Braak <38856699+Mathijstb@users.noreply.github.com>
2022-12-06 09:01:05 +0100

    day6

--- 2022/src/main/java/day6/Day6.java
+++ 2022/src/main/java/day6/Day6.java
@@ -1,11 +1,29 @@
 package day6;
 
 import fileUtils.FileReader;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day6 {
 
     public static void execute() {
         List<String> lines = FileReader.getFileReader().readFile("input6.csv");
+        var line = lines.stream().findFirst().orElseThrow();
+        findFirstMarker(line, 4);
+        findFirstMarker(line, 14);
+    }
+
+    private static void findFirstMarker(String line, int size) {
+        for (int i = 0; i < line.length()-size; i++) {
+            var characterSet = line.substring(i, i + size)
+                    .chars()
+                    .mapToObj(chr -> (char) chr)
+                    .collect(Collectors.toSet());
+            if (characterSet.size() == size) {
+                System.out.println("Index: " + (i + size));
+                break;
+            }
+        }
     }
 }
