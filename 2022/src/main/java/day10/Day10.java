ff563832487b6a78d651688f321e248395d41ab9
Mathijs ter Braak <38856699+Mathijstb@users.noreply.github.com>
2022-12-09 09:44:46 +0100

    day10 init

---
+++ 2022/src/main/java/day10/Day10.java
@@ -0,0 +1,12 @@
+package day10;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day10 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input10.csv");
+    }
+
+}
