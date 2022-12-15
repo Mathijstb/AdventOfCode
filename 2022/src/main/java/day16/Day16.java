efad901677b61474cb71f75cfb7ff0357c662736
Mathijs ter Braak <38856699+Mathijstb@users.noreply.github.com>
2022-12-15 20:34:30 +0100

    day 16 init

---
+++ 2022/src/main/java/day16/Day16.java
@@ -0,0 +1,10 @@
+package day16;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day16 {
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input15.csv");
+    }
+}
