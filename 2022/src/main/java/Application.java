dc10d4cb2ef6e9e86e720d80a27e392b5131f257
Mathijs ter Braak <38856699+Mathijstb@users.noreply.github.com>
2022-12-18 21:01:12 +0100

    day 19 init

--- 2022/src/main/java/Application.java
+++ 2022/src/main/java/Application.java
@@ -1,9 +1,8 @@
-import day17.Day17;
-import day18.Day18;
+import day19.Day19;
 
 public class Application {
 
     public static void main(String [] args) {
-        Day18.execute();
+        Day19.execute();
     }
 }
---
+++ 2022/src/main/java/day19/Day19.java
@@ -0,0 +1,12 @@
+package day19;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day19 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input19.csv");
+    }
+
+}
