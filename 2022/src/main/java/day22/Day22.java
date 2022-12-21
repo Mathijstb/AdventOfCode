49c5a63a7b6e6cc68af0c6e43e98d58a1d54f599
Mathijs ter Braak <38856699+Mathijstb@users.noreply.github.com>
2022-12-21 20:57:39 +0100

    day 22 init

---
+++ 2022/src/main/java/day22/Day22.java
@@ -0,0 +1,12 @@
+package day22;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day22 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input22.csv");
+    }
+
+}
