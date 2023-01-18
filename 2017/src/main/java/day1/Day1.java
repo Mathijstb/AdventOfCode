80fe5e3cb5d2d9dd8551ad070ee9eedba6781a85
Mathijs ter Braak <38856699+Mathijstb@users.noreply.github.com>
2023-01-18 22:53:18 +0100

    Day1 init

--- 2017/src/main/java/day1/Day1.java
+++ 2017/src/main/java/day1/Day1.java
@@ -1,8 +1,11 @@
 package day1;
 
+import fileUtils.FileReader;
+import java.util.List;
+
 public class Day1 {
 
     public static void execute() {
-
+        List<String> lists = new FileReader().readFile("input1.csv");
     }
 }
