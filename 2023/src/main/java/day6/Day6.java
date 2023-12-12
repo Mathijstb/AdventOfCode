d479b70ff68e281a66a5906a17308105a2044569
Mathijs ter Braak <mterbraak@ilionx.com>
2023-12-12 11:01:38 +0100

    Day 6 init

--- 2023/src/main/java/day6/Day6.java
+++ 2023/src/main/java/day6/Day6.java
@@ -3,7 +3,6 @@ package day6;
 import fileUtils.FileReader;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
---
+++ 2023/src/main/java/day7/Day7.java
@@ -0,0 +1,11 @@
+package day7;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day7 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input7.csv");
+    }
+}
