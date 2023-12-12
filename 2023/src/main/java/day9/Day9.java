6d17fe9f787f941073a16e082cf925b59a88ae3c
Mathijs ter Braak <mterbraak@ilionx.com>
2023-12-12 21:07:28 +0100

    Day 9 init

---
+++ 2023/src/main/java/day9/Day9.java
@@ -0,0 +1,11 @@
+package day9;
+
+import fileUtils.FileReader;
+import java.util.*;
+
+public class Day9 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input9.csv");
+    }
+}
