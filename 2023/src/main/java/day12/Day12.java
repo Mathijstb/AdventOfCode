fd92b8f5777904fba2a669562479931caa4b4c02
Mathijs ter Braak <mterbraak@ilionx.com>
2023-12-14 23:34:06 +0100

    Day 12 init

---
+++ 2023/src/main/java/day12/Day12.java
@@ -0,0 +1,11 @@
+package day12;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day12 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input12.csv");
+    }
+}
