972071c309b32b140bca857204eac393c0507fd0
Mathijs ter Braak <mterbraak@ilionx.com>
2023-12-22 16:07:41 +0100

    Day 14 init

---
+++ 2023/src/main/java/day14/Day14.java
@@ -0,0 +1,11 @@
+package day14;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day14 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input14.csv");
+    }
+}
