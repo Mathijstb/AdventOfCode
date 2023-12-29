c53cc8347113ccad968744544aeac84ce5358539
Mathijs ter Braak <mterbraak@ilionx.com>
2023-12-29 22:21:06 +0100

    Day 17 init

---
+++ 2023/src/main/java/day17/Day17.java
@@ -0,0 +1,12 @@
+package day17;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day17 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input17.csv");
+    }
+
+}
