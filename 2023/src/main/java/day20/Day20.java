0932e328e441aa30b41418153326d44913bbe243
Mathijs ter Braak <mterbraak@ilionx.com>
2024-01-19 17:31:25 +0100

    Day 20 init

---
+++ 2023/src/main/java/day20/Day20.java
@@ -0,0 +1,12 @@
+package day20;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day20 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input20.csv");
+    }
+
+}
