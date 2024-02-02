f292baf8c60b70636257f474626ff04b408e39af
Mathijs ter Braak <mterbraak@ilionx.com>
2024-02-02 12:40:41 +0100

    Day 22 init

---
+++ 2023/src/main/java/day22/Day22.java
@@ -0,0 +1,13 @@
+package day22;
+
+import fileUtils.FileReader;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Day22 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input22.csv");
+    }
+}
