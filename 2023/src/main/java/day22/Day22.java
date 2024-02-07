5a201ec63c095463d80dafb095de1e0cb99096c1
Mathijs ter Braak <mterbraak@ilionx.com>
2024-02-07 22:30:07 +0100

    Day 22

--- 2023/src/main/java/day22/Day22.java
+++ 2023/src/main/java/day22/Day22.java
@@ -1,13 +1,99 @@
 package day22;
 
 import fileUtils.FileReader;
+import grids.Grid3D;
 
+import javax.vecmath.Point3i;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static grids.Grid3D.Dimension.*;
 
 public class Day22 {
 
     public static void execute() {
-        List<String> lines = FileReader.getFileReader().readFile("input22.csv");
+        var lines = FileReader.getFileReader().readFile("input22.csv");
+        var bricks = parseBricks(lines);
+        var grid = parseGrid(bricks);
+        drawGrid(grid);
+        dropBricks(grid, bricks);
+        drawGrid(grid);
+        printBricksBelow(grid, bricks);
+        System.out.println();
+        printBricksAbove(grid, bricks);
+        var numberOfBricksDisintegratable = bricks.stream().filter(brick -> {
+            var bricksAbove = brick.getBricksAbove(grid);
+            return bricksAbove.stream().allMatch(brickAbove -> (long) brickAbove.getBricksBelow(grid).size() > 1);
+        }).count();
+        System.out.println();
+        System.out.println("Number of bricks that can safely be disintegrated: " + numberOfBricksDisintegratable);
+    }
+
+    private static void printBricksBelow(Grid3D<PointState> grid, List<Brick> bricks) {
+        bricks.forEach(brick -> {
+            var bricksBelow = brick.getBricksBelow(grid);
+            var bricksBelowString = bricksBelow.isEmpty() ? "-" : bricksBelow.stream()
+                    .map(b -> String.valueOf(b.getId())).collect(Collectors.joining(", "));
+            System.out.printf("Brick %s is supported by: %s%n", brick.getId(), bricksBelowString);
+        });
+    }
+
+    private static void printBricksAbove(Grid3D<PointState> grid, List<Brick> bricks) {
+        bricks.forEach(brick -> {
+            var bricksAbove = brick.getBricksAbove(grid);
+            var bricksAboveString = bricksAbove.isEmpty() ? "-" : bricksAbove.stream()
+                    .map(b -> String.valueOf(b.getId())).collect(Collectors.joining(", "));
+            System.out.printf("Brick %s is supporting: %s%n", brick.getId(), bricksAboveString);
+        });
+    }
+
+    private static final Function<PointState, String> drawFunction = pointState -> String.valueOf(pointState.brick().getId());
+
+    private static void drawGrid(Grid3D<PointState> grid) {
+        grid.draw(X, Z, drawFunction);
+        System.out.println();
+        grid.draw(Y, Z, drawFunction);
+        System.out.println();
+    }
+
+    private static void dropBricks(Grid3D<PointState> grid, List<Brick> bricks) {
+        List<Brick> currentBricks = new ArrayList<>(bricks);
+        currentBricks.sort(Comparator.comparing(brick -> brick.getLowestPoint().z));
+        while (!currentBricks.isEmpty()) {
+            var currentBrick = currentBricks.remove(0);
+            if (!currentBrick.isFloating(grid)) {
+                continue;
+            }
+            while (currentBrick.isFloating(grid)) {
+                var oldPoints = currentBrick.getPoints();
+                var newPoints = oldPoints.stream().map(point -> new Point3i(point.x, point.y, point.z - 1)).toList();
+                oldPoints.forEach(grid::remove);
+                newPoints.forEach(newPoint -> grid.setValue(newPoint, new PointState(currentBrick)));
+                currentBrick.setPoints(newPoints);
+            }
+        }
+    }
+
+    private static List<Brick> parseBricks(List<String> lines) {
+        var id = new AtomicInteger(0);
+        return lines.stream().map(line -> {
+            var points = Arrays.stream(line.split("~"))
+                    .map(part -> part.split(","))
+                    .map(coords -> new Point3i(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]), Integer.parseInt(coords[2])))
+                    .toList();
+            char characterId = (char) ('A' + id.getAndIncrement());
+            return new Brick(characterId, points.get(0), points.get(1));
+        }).toList();
+    }
+
+    private static Grid3D<PointState> parseGrid(List<Brick> bricks) {
+        var grid = new Grid3D<PointState>();
+        bricks.forEach(brick -> brick.getPoints().forEach(point -> grid.setValue(point, new PointState(brick))));
+        return grid;
     }
 }
--- utility/src/main/java/grids/Grid3D.java
+++ utility/src/main/java/grids/Grid3D.java
@@ -1,11 +1,18 @@
 package grids;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.vecmath.Point3i;
+import java.awt.*;
 import java.util.*;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Grid3D<T> {
 
-    public final Map<Point3i, T> points = new HashMap<>();
+    private final Map<Point3i, T> points = new HashMap<>();
 
     public T getValue(Point3i point) {
         return points.get(point);
@@ -13,7 +20,6 @@ public class Grid3D<T> {
 
     public boolean containsPoint(Point3i point) {
         return points.containsKey(point);
-
     }
 
     public Set<Point3i> getAllPoints() {
@@ -48,26 +54,85 @@ public class Grid3D<T> {
         points.put(point, value);
     }
 
-    public Set<Point3i> getNeighbours(Point3i point, boolean includeDiagonals) {
-        Set<Point3i> neighbours = new HashSet<>();
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dy = -1; dy <= 1; dy++) {
-                for (int dz = -1; dz <= 1; dz++) {
-                    int newX = point.x + dx;
-                    int newY = point.y + dy;
-                    int newZ = point.z + dz;
+    public void remove(Point3i point) {
+        points.remove(point);
+    }
 
-                    if (dx == 0 && dy == 0 && dz == 0) continue;
-                    if (!includeDiagonals) {
-                        if ((dx != 0 && dy != 0) || (dx != 0 && dz != 0) || (dy != 0 && dz != 0)) continue;
-                    }
+    private static final Set<Point3i> neighboursExcludingDiagonalsDiffSet = Set.of(
+            new Point3i(-1, 0, 0),
+            new Point3i(1, 0, 0),
+            new Point3i(0, -1, 0),
+            new Point3i(0, 1, 0),
+            new Point3i(0, 0, -1),
+            new Point3i(0, 0, 1));
 
-                    var newPoint = new Point3i(newX, newY, newZ);
-                    if (points.containsKey(newPoint)) neighbours.add(newPoint);
-                }
-            }
-        }
-        return neighbours;
+    private static final Set<Point3i> neighboursIncludingDiagonalsDiffSet = Set.of(
+            new Point3i(-1, 0, 0),
+            new Point3i(1, 0, 0),
+            new Point3i(0, -1, 0),
+            new Point3i(0, 1, 0),
+            new Point3i(0, 0, -1),
+            new Point3i(0, 0, 1),
+            new Point3i(-1, -1, -1),
+            new Point3i(1, -1, -1),
+            new Point3i(-1, 1, -1),
+            new Point3i(-1, -1, 1),
+            new Point3i(1, 1, -1),
+            new Point3i(-1, 1, 1),
+            new Point3i(1, -1, 1),
+            new Point3i(1, 1, 1));
+
+    public List<Point3i> getNeighbours(Point3i point, boolean includeDiagonals) {
+        return (includeDiagonals ? neighboursExcludingDiagonalsDiffSet : neighboursIncludingDiagonalsDiffSet).stream()
+                .map(diff -> new Point3i(point.x + diff.x, point.y + diff.y, point.z + diff.z))
+                .filter(points::containsKey).toList();
+    }
+
+    public enum Dimension {
+        X,
+        Y,
+        Z
+    }
+
+    public void draw(Dimension dim1, Dimension dim2, Function<T, String> toStringFunction) {
+        var dim3 = Arrays.stream(Dimension.values()).filter(dim -> !Set.of(dim1, dim2).contains(dim)).findFirst().orElseThrow();
+        var dimProjection1 = getDimensionProjection(dim1);
+        var dimProjection2 = getDimensionProjection(dim2);
+        var dimProjection3 = getDimensionProjection(dim3);
+
+        //Collect all points in the dim1-dim2 surface with minimal dim3
+        var points = getAllPoints().stream()
+                .collect(Collectors.groupingBy(p -> new Point(dimProjection1.apply(p), dimProjection2.apply(p))))
+                .entrySet().stream()
+                .map(entry -> Map.entry(entry.getKey(), entry.getValue().stream()
+                        .min(Comparator.comparingInt(dimProjection3::apply)).orElseThrow()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        var minDimension1 = points.keySet().stream().min(Comparator.comparingInt(p -> p.x)).orElseThrow().x;
+        var maxDimension1 = points.keySet().stream().max(Comparator.comparingInt(p -> p.x)).orElseThrow().x;
+        var minDimension2 = points.keySet().stream().min(Comparator.comparingInt(p -> p.y)).orElseThrow().y;
+        var maxDimension2 = points.keySet().stream().max(Comparator.comparingInt(p -> p.y)).orElseThrow().y;
+
+        var width = maxDimension1 - minDimension1 + 1;
+        var height = maxDimension2 - minDimension2 + 1;
+        System.out.printf("%s%s%s%n", StringUtils.repeat(' ', width / 2), dim1, StringUtils.repeat(' ', width / 2));
+        System.out.println(StringUtils.repeat('-', width));
+        IntStream.range(minDimension2, maxDimension2 + 1).map(v2 -> maxDimension2 - (v2 - minDimension2)).forEach(v2 -> {
+            var printRow = IntStream.range(minDimension1, maxDimension1 + 1)
+                    .mapToObj(v1 -> new Point(v1, v2))
+                    .map(point -> points.containsKey(point) ? toStringFunction.apply(getValue(points.get(point))) : ".")
+                    .collect(Collectors.joining());
+            var suffix = (v2 == height / 2 + 1) ? " | " + dim2 : " |";
+            System.out.println(printRow + suffix);
+        });
+    }
+
+    private Function<Point3i, Integer> getDimensionProjection(Dimension dimension) {
+        return switch (dimension) {
+            case X -> p -> p.x;
+            case Y -> p -> p.y;
+            case Z -> p -> p.z;
+        };
     }
 
 
