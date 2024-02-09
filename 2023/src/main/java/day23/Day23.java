3f039e2a92939527500f7fdb4065e08cd4781dc7
Mathijs ter Braak <mterbraak@ilionx.com>
2024-02-09 15:30:53 +0100

    Day 23 init

---
+++ 2023/src/main/java/day23/Day23.java
@@ -0,0 +1,11 @@
+package day23;
+
+import fileUtils.FileReader;
+
+public class Day23 {
+
+    public static void execute() {
+        var lines = FileReader.getFileReader().readFile("input23.csv");
+    }
+
+}
