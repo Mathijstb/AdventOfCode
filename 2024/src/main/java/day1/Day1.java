c1bec968c7cbd63c03ff1f4731c6606ff29077cb
Mathijs ter Braak <mterbraak@ilionx.com>
2024-12-02 06:46:14 +0100

    Day 1

--- 2024/src/main/java/day1/Day1.java
+++ 2024/src/main/java/day1/Day1.java
@@ -7,5 +7,31 @@ public class Day1 {
 
     public static void execute() {
         List<String> lines = FileReader.getFileReader().readFile("input1.csv");
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        lines.forEach(line -> {
+            var numbers = line.split("   ");
+            list1.add(Integer.parseInt(numbers[0]));
+            list2.add(Integer.parseInt(numbers[1]));
+        });
+        findTotalDistance(list1, list2);
+        findSimilarityScore(list1, list2);
+    }
+
+    private static void findTotalDistance(List<Integer> list1, List<Integer> list2) {
+        int totalDistance  = 0;
+        list1.sort(Comparator.comparingInt(i -> i));
+        list2.sort(Comparator.comparingInt(i -> i));
+        for (int i = 0; i < list1.size(); i++) {
+            totalDistance += Math.abs(list1.get(i) - list2.get(i));
+        }
+        System.out.println("Total distance: " + totalDistance);
+    }
+
+    private static void findSimilarityScore(List<Integer> list1, List<Integer> list2) {
+        var totalScore = list1.stream()
+                .map(i -> i * list2.stream().filter(j -> j.equals(i)).count()
+        ).reduce(Long::sum).orElseThrow();
+        System.out.println("Total score: " + totalScore);
     }
 }
