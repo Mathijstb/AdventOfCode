48057d490c06b4dae967d54d0378c3b9dc5aa63c
Mathijs ter Braak <mterbraak@ilionx.com>
2024-12-02 06:47:20 +0100

    Day 2 init

---
+++ 2024/src/main/java/day2/Day2.java
@@ -0,0 +1,11 @@
+package day2;
+
+import fileUtils.FileReader;
+import java.util.List;
+
+public class Day2 {
+
+    public static void execute() {
+        List<String> lines = FileReader.getFileReader().readFile("input2.csv");
+    }
+}
